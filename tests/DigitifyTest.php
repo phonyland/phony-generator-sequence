bc68ab5c8b767946a84b81c393ca7b5bf2862a86
Yunus Emre Deligöz <ye@deligoz.me>
2022-03-17 20:40:12 +0300

    DigitifyTest: Update test names

--- tests/DigitifyTest.php
+++ tests/DigitifyTest.php
@@ -1,13 +1,13 @@
 <?php
 
-it('can replace hash signs into random digits')
+it('can replace hash signs into with-zero random digits')
     ->expect(🙃()->sequence->digitify('###'))
     ->toMatch('/^[\d]{0,3}$/');
 
-it('can replace hash signs into random digits with other strings')
+it('can replace hash signs into with-zero random digits with other strings')
     ->expect(🙃()->sequence->digitify('test ###### test'))
     ->toMatch('/^test [\d]{0,6} test$/');
 
-it('can replace hash signs into random digits with other strings without spaces')
+it('can replace hash signs into with-zero random digits with other strings without spaces')
     ->expect(🙃()->sequence->digitify('test######test'))
     ->toMatch('/^test[\d]{0,6}test$/');

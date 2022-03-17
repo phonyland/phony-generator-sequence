8c49a996ba433d014e826c27eb571f3aaebeb162
Yunus Emre Deligöz <ye@deligoz.me>
2022-03-17 20:43:25 +0300

    Introduce a Pipe mechanism

---
+++ src/Pipe.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phonyland\SequenceGenerator;
+
+class Pipe
+{
+    private function __construct(public mixed $value) {}
+
+    public static function new(mixed $value): self { return new self($value); }
+
+    public function __toString(): string { return (string) $this->value; }
+
+    public function __invoke(callable $param): static
+    {
+        $this->value = $param($this->value);
+
+        return $this;
+    }
+}

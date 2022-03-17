5d2db085bc91e9b74d468fd63690c396512ae44d
Yunus Emre Deligöz <ye@deligoz.me>
2022-03-17 20:40:40 +0300

    DigitifyNonZeroTest: Fix match expressions

--- tests/DigitifyNonZeroTest.php
+++ tests/DigitifyNonZeroTest.php
@@ -2,12 +2,12 @@
 
 it('can replace percentage signs into non-zero random digits')
     ->expect(🙃()->sequence->digitifyNonZero('%%%'))
-    ->toMatch('/^[\d]{0,3}$/');
+    ->toMatch('/^[1-9]{0,3}$/');
 
 it('can replace percentage signs into non-zero random digits with other strings')
     ->expect(🙃()->sequence->digitifyNonZero('test %%%%%% test'))
-    ->toMatch('/^test [\d]{0,6} test$/');
+    ->toMatch('/^test [1-9]{0,6} test$/');
 
 it('can replace percentage signs into non-zero random digits with other strings without spaces')
     ->expect(🙃()->sequence->digitifyNonZero('test%%%%%%test'))
-    ->toMatch('/^test[\d]{0,6}test$/');
+    ->toMatch('/^test[1-9]{0,6}test$/');

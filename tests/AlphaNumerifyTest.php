5a1e70de42147499136655730c1d5ed547a35687
Yunus Emre Deligöz <ye@deligoz.me>
2022-03-24 20:43:55 +0300

    AlphaNumerifyTest: Fix dataset name

--- tests/AlphaNumerifyTest.php
+++ tests/AlphaNumerifyTest.php
@@ -3,14 +3,14 @@
 it('can alpha numerify sequence characters', function (string $expression, string $sign) {
     expect(🙃()->sequence->alphanumerify(str_repeat($sign, 100)))
         ->toMatch("/^$expression\$/");
-})->with('signs');
+})->with('alphanumerify');
 
 it('can alpha numerify sequence characters with other strings', function (string $expression, string $sign) {
     expect(🙃()->sequence->alphanumerify('test ' . str_repeat($sign, 100) . ' test'))
         ->toMatch("/^test $expression test\$/");
-})->with('signs');
+})->with('alphanumerify');
 
 it('can alpha numerify sequence characters with other strings without spaces', function (string $expression, string $sign) {
     expect(🙃()->sequence->alphanumerify('test' . str_repeat($sign, 100) . 'test'))
         ->toMatch("/^test{$expression}test\$/");
-})->with('signs');
+})->with('alphanumerify');
